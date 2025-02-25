161c30c9521dbb8e174ac72ab4fdfe18cdd8a015
Brandon Ochia <brandon.ochia@gmail.com>
2025-02-24 21:39:11 -0600

    feat: get a starting table for the directory page.

--- stargate.client/src/app/person-directory/person-directory.component.ts
+++ stargate.client/src/app/person-directory/person-directory.component.ts
@@ -6,5 +6,53 @@ import { Component } from '@angular/core';
   styleUrl: './person-directory.component.css'
 })
 export class PersonDirectoryComponent {
+  people: PersonAstronautDto[] = [
+    {
+      personId: 1,
+      name: 'Neil Armstrong',
+      currentRank: 'Commander',
+      currentDutyTitle: 'Apollo 11',
+      careerStartDate: new Date('1966-03-16'),
+      careerEndDate: new Date('1969-07-24')
+    },
+    {
+      personId: 2,
+      name: 'Buzz Aldrin',
+      currentRank: 'Lunar Module Pilot',
+      currentDutyTitle: 'Apollo 11',
+      careerStartDate: new Date('1966-03-16'),
+      careerEndDate: new Date('1969-07-24')
+    },
+    {
+      personId: 3,
+      name: 'Sally Ride',
+      currentRank: 'Mission Specialist',
+      currentDutyTitle: 'STS-7',
+      careerStartDate: new Date('1978-01-16'),
+      careerEndDate: new Date('1987-08-15')
+    },
+    {
+      personId: 4,
+      name: 'Chris Hadfield',
+      currentRank: 'Commander',
+      currentDutyTitle: 'ISS Commander',
+      careerStartDate: new Date('1992-06-07'),
+      careerEndDate: new Date('2013-07-03')
+    }
+  ];
 
+  constructor() { }
+
+  ngOnInit(): void { }
+}
+
+
+// ochia - where should I put this?
+export interface PersonAstronautDto {
+  personId: number;
+  name: string;
+  currentRank?: string;
+  currentDutyTitle?: string;
+  careerStartDate?: Date;
+  careerEndDate?: Date;
 }
